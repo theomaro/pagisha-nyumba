be86cabd26ed937611909d6d3aca4344e00b8ecd
Mathew Maro <149648717+theomaro@users.noreply.github.com>
2024-06-25 18:42:50 +0300

    add save area view to all screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="bg-blue-500 h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full min-h-screen justify-center items-center px-4">
+          <Text className="text-sky-100">User profile</Text>
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="white" style="dark" />
+    </SafeAreaView>
   );
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Pangisha Nyumba</Text>
-    </View>
+    <SafeAreaView className="bg-blue-500 h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full min-h-screen justify-center items-center px-4">
+          <Text className="text-sky-100">Pangisha Nyumba</Text>
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="white" style="dark" />
+    </SafeAreaView>
   );
 }
