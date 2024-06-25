fa0924cd5fd617e3e8a1b7b105c33f808ec28101
Mathew Maro <149648717+theomaro@users.noreply.github.com>
2024-06-25 17:41:18 +0300

    define auth route

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>Sign In</Text>
+    </View>
+  );
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>Sign Up</Text>
+    </View>
+  );
+};
+
+export default SignUp;
