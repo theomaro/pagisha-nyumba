3cf319962485254f3591897f31b0f446cd8779fa
Mathew Maro <149648717+theomaro@users.noreply.github.com>
2024-06-26 11:27:09 +0300

    add signup screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -22,59 +22,57 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-slate-100 h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full min-h-screen justify-center gap-y-28 px-4">
-          <View className="w-full">
-            <Text className="text-3xl text-sky-700 font-bold max-w-[300px]">
-              Log in to Pangisha
+        <View className="w-full my-20 px-4">
+          <Text className="text-2xl text-sky-700 font-bold max-w-[300px]">
+            Log in to your account
+          </Text>
+        </View>
+
+        <View className="w-full px-4">
+          <FormField
+            label="Email"
+            value={formData.email}
+            placeholder=""
+            keyboardType="email-address"
+            onChangeHandler={(text) =>
+              setformData({
+                email: text,
+                password: formData.password,
+              })
+            }
+          />
+
+          <FormField
+            label="Password"
+            value={formData.password}
+            placeholder=""
+            fieldStyles="mt-7"
+            onChangeHandler={(text) =>
+              setformData({
+                email: formData.email,
+                password: text,
+              })
+            }
+          />
+
+          <ActionButton
+            title="Sign In"
+            containerStyles="w-full mt-7"
+            isLoading={isSubmitting}
+            onPressHandler={handleSubmit}
+          />
+
+          <View className="flex-row items-center gap-2 p-1">
+            <Text className="text-slate-800 text-lg">
+              Don't have an account?
             </Text>
-          </View>
 
-          <View className="w-full">
-            <FormField
-              label="Email"
-              value={formData.email}
-              placeholder=""
-              keyboardType="email-address"
-              onChangeHandler={(text) =>
-                setformData({
-                  email: text,
-                  password: formData.password,
-                })
-              }
-            />
-
-            <FormField
-              label="Password"
-              value={formData.password}
-              placeholder=""
-              fieldStyles="mt-7"
-              onChangeHandler={(text) =>
-                setformData({
-                  email: formData.email,
-                  password: text,
-                })
-              }
-            />
-
-            <ActionButton
-              title="Sign In"
-              containerStyles="w-full mt-7"
-              isLoading={isSubmitting}
-              onPressHandler={handleSubmit}
-            />
-
-            <View className="flex-row items-center gap-2 p-1">
-              <Text className="text-slate-800 text-lg">
-                Don't have an account?
-              </Text>
-
-              <Link
-                href="/sign-up"
-                className="underline font-bold text-sky-800 text-lg"
-              >
-                Sign Up
-              </Link>
-            </View>
+            <Link
+              href="/sign-up"
+              className="underline font-bold text-sky-800 text-lg"
+            >
+              Sign Up
+            </Link>
           </View>
         </View>
       </ScrollView>
@@ -83,5 +81,5 @@ const SignIn = () => {
     </SafeAreaView>
   );
 };
-
+// min-h-screen justify-center px-4
 export default SignIn;
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,19 +1,103 @@
+import ActionButton from "@/components/ActionButton";
+import FormField from "@/components/FormField";
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { View, Text, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
-const SignUp = () => {
+const SignIn = () => {
+  const [formData, setformData] = useState<{
+    fullname: string;
+    email: string;
+    password: string;
+  }>({
+    fullname: "",
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = () => {};
+
   return (
-    <SafeAreaView className="bg-blue-500 h-full">
+    <SafeAreaView className="bg-slate-100 h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full min-h-screen justify-center items-center px-4">
-          <Text className="text-sky-100">Sign Up</Text>
+        <View className="w-full my-20 px-4">
+          <Text className="text-2xl text-sky-700 font-bold max-w-[300px]">
+            Register new account
+          </Text>
+        </View>
+
+        <View className="w-full px-4">
+          <FormField
+            label="Name"
+            value={formData.fullname}
+            placeholder=""
+            onChangeHandler={(text) =>
+              setformData({
+                fullname: text,
+                password: formData.password,
+                email: formData.email,
+              })
+            }
+          />
+
+          <FormField
+            label="Email"
+            value={formData.email}
+            placeholder=""
+            fieldStyles="mt-5"
+            keyboardType="email-address"
+            onChangeHandler={(text) =>
+              setformData({
+                email: text,
+                password: formData.password,
+                fullname: formData.fullname,
+              })
+            }
+          />
+
+          <FormField
+            label="Password"
+            value={formData.password}
+            placeholder=""
+            fieldStyles="mt-5"
+            onChangeHandler={(text) =>
+              setformData({
+                email: formData.email,
+                password: text,
+                fullname: formData.fullname,
+              })
+            }
+          />
+
+          <ActionButton
+            title="Sign Up"
+            containerStyles="w-full mt-7"
+            isLoading={isSubmitting}
+            onPressHandler={handleSubmit}
+          />
+
+          <View className="flex-row items-center gap-2 p-1">
+            <Text className="text-slate-800 text-lg">
+              Already have an account?
+            </Text>
+
+            <Link
+              href="/sign-in"
+              className="underline font-bold text-sky-800 text-lg"
+            >
+              Sign In
+            </Link>
+          </View>
         </View>
       </ScrollView>
 
-      <StatusBar backgroundColor="white" style="dark" />
+      <StatusBar backgroundColor="#0369a1" style="light" translucent={true} />
     </SafeAreaView>
   );
 };
 
-export default SignUp;
+export default SignIn;
