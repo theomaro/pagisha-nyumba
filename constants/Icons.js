992078f439c532b51ddef4fd4e0637e431c1e10a
Mathew Maro <149648717+theomaro@users.noreply.github.com>
2024-06-26 17:19:41 +0300

    load tab icons

--- constants/Icons.js
+++ constants/Icons.js
@@ -1,8 +1,13 @@
 import eye from "../assets/icons/eye.png";
-
 import eyeHide from "../assets/icons/eye-hide.png";
+import profile from "../assets/icons/profile.png";
+import bookmark from "../assets/icons/bookmark.png";
+import home from "../assets/icons/home.png";
 
 export default {
   eye,
   eyeHide,
+  profile,
+  bookmark,
+  home,
 };
